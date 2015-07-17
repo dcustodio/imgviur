9525776dcb8bd8be4b6078a6f987d4e752944e5d
David Custodio <davidlacustodio@gmail.com>
2015-07-17 19:25:01 +0100

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name imgviurApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the imgviurApp
+ */
+angular.module('imgviurApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name imgviurApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the imgviurApp
+ */
+angular.module('imgviurApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
